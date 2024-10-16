29f4de1b8f4684c55f81a0085b63aad6cb97c940
João Paulo Hora <joaopaulo8hora@gmail.com>
2024-10-15 22:44:36 -0300

    Criando arquivos do projeto

---
+++ src/main/java/com/game_class/controllers/PageController.java
@@ -0,0 +1,18 @@
+package com.game_class.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
---
+++ src/test/java/com/game_class/GameClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game_class;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameClassApplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
