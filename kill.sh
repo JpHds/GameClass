b64fb1fdd972b5e643588c74a9b47c2597a4cf3d
João Paulo Hora <joaopaulo8hora@gmail.com>
2024-10-16 00:42:03 -0300

    subindo arquivos .sh para inicializar e parar a aplicação

---
+++ kill.sh
@@ -0,0 +1,8 @@
+# Comando para parar a aplicação Spring Boot
+echo "Parando a aplicação Spring Boot..."
+./mvnw spring-boot:stop
+
+# Comando para manter o console ativo após a execução
+echo "Aplicação parada com sucesso."
+echo "Pressione [Enter] para sair..."
+read
---
+++ start.sh
@@ -0,0 +1,7 @@
+# Comando para iniciar a aplicação Spring Boot
+echo "Iniciando a aplicação Game Class..."
+./mvnw spring-boot:start
+
+# Comando para manter o console ativo após a execução
+echo "Pressione [Enter] para sair..."
+read
