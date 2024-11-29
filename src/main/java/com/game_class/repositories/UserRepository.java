3e937285b72b2974bc118b8b1e6499c08cb41384
João Paulo Hora <joaopaulo8hora@gmail.com>
2024-11-29 14:12:54 -0300

    Teste para o SQL

--- src/main/java/com/game_class/repositories/UserRepository.java
+++ src/main/java/com/game_class/repositories/UserRepository.java
@@ -14,11 +14,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 
     @Query("SELECT new com.game_class.dtos.UsersRankingDTO(" +
-           "u.username, SUM(v.voteValue)) " +
-           "FROM User u " +
-           "JOIN Comment c ON c.user = u " +
-           "JOIN Vote v ON v.comment = c " +
-           "GROUP BY u.userId, u.username " +
-           "ORDER BY SUM(v.voteValue) DESC")
+            "u.username, SUM(v.voteValue)) " +
+            "FROM User u " +
+            "JOIN Comment c ON c.user = u " +
+            "JOIN Vote v ON v.comment = c " +
+            "GROUP BY u.userId, u.username " +
+            "ORDER BY SUM(v.voteValue) DESC")
     List<UsersRankingDTO> findUsersRank();
+
 }
